56631d0f3b7d9799969bcbf74a6a794ff39921c5
= <mamykin.av@bk.ru>
2018-03-20 21:26:38 +0500

    Перевод на Kotlin, прочее

---
+++ app/src/main/java/ru/mamykin/foreignbooksreader/ReaderApp.kt
@@ -0,0 +1,57 @@
+package ru.mamykin.foreignbooksreader
+
+import android.support.multidex.MultiDexApplication
+
+import ru.mamykin.foreignbooksreader.common.UiUtils
+import ru.mamykin.foreignbooksreader.di.AppComponent
+import ru.mamykin.foreignbooksreader.di.modules.AppModule
+import ru.mamykin.foreignbooksreader.di.DaggerAppComponent
+import ru.mamykin.foreignbooksreader.di.modules.PreferencesModule
+import ru.mamykin.foreignbooksreader.preferences.PreferencesManager
+
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
+
+import javax.inject.Inject
+
+import io.fabric.sdk.android.Fabric
+import ru.mamykin.foreignbooksreader.preferences.PreferenceNames
+
+class ReaderApp : MultiDexApplication(), PreferenceNames {
+    @Inject
+    protected var pm: PreferencesManager? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        setupAppComponent()
+        setupCrashlytics()
+        setupTheme()
+    }
+
+    private fun setupCrashlytics() {
+        // Отключем Crashlytics для Debug-сборок
+        val core = CrashlyticsCore.Builder()
+                .disabled(BuildConfig.DEBUG).build()
+        val crashlytics = Crashlytics.Builder()
+                .core(core)
+                .build()
+        Fabric.with(this, crashlytics)
+    }
+
+    private fun setupTheme() {
+        UiUtils.setNightMode(pm!!.getBoolean(PreferenceNames.Companion.NIGHT_THEME_PREF))
+    }
+
+    private fun setupAppComponent() {
+        component = DaggerAppComponent.builder()
+                .appModule(AppModule(this))
+                .preferencesModule(PreferencesModule())
+                .build()
+        component!!.inject(this)
+    }
+
+    companion object {
+        var component: AppComponent? = null
+            private set
+    }
+}
