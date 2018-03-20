4f9e82aad90a9648378437790ddf723ef9249edd
= <mamykin.av@bk.ru>
2018-03-20 21:26:10 +0500

    Перевод пакета ui.views на Kotlin, часть 2

---
+++ app/src/main/java/ru/mamykin/foreignbooksreader/views/SplashView.kt
@@ -0,0 +1,10 @@
+package ru.mamykin.foreignbooksreader.views
+
+import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
+
+@StateStrategyType(AddToEndSingleStrategy::class)
+interface SplashView : MvpView {
+    fun startApplication()
+}
