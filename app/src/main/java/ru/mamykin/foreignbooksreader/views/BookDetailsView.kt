4fed44d0d0898b05a8d1e002ed2a2964c8f7f745
= <mamykin.av@bk.ru>
2018-03-20 21:25:35 +0500

    Перевод пакета ui.views на Kotlin

---
+++ app/src/main/java/ru/mamykin/foreignbooksreader/views/BookDetailsView.kt
@@ -0,0 +1,28 @@
+package ru.mamykin.foreignbooksreader.views
+
+import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
+
+@StateStrategyType(AddToEndSingleStrategy::class)
+interface BookDetailsView : MvpView {
+    fun openBook(bookId: Int)
+
+    fun showTitle(title: String)
+
+    fun setHomeEnabled(enabled: Boolean)
+
+    fun showBookName(name: String)
+
+    fun showBookAuthor(author: String)
+
+    fun showBookPath(path: String)
+
+    fun showBookCurrentPage(currentPage: String)
+
+    fun showBookGenre(genre: String)
+
+    fun showBookOriginalLang(lang: String)
+
+    fun showBookCreatedDate(date: String)
+}
---
+++ app/src/main/java/ru/mamykin/foreignbooksreader/views/DeviceBooksView.kt
@@ -0,0 +1,20 @@
+package ru.mamykin.foreignbooksreader.views
+
+import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
+
+import ru.mamykin.foreignbooksreader.models.AndroidFile
+
+@StateStrategyType(AddToEndSingleStrategy::class)
+interface DeviceBooksView : MvpView {
+    fun showFiles(files: List<AndroidFile>)
+
+    fun setCurrentDir(currentDir: String)
+
+    fun showPermissionMessage()
+
+    fun openBook(path: String)
+
+    fun showUpDir(show: Boolean)
+}
