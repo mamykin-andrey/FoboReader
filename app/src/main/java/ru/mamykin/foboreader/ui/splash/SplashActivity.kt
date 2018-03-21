551d499ddf27c5d8f104f23a254db354d2bc02ed
= <mamykin.av@bk.ru>
2018-03-22 00:28:57 +0500

    refactor splash screenn

--- app/src/main/java/ru/mamykin/foboreader/ui/splash/SplashActivity.kt
+++ app/src/main/java/ru/mamykin/foboreader/ui/splash/SplashActivity.kt
@@ -1,29 +1,16 @@
 package ru.mamykin.foboreader.ui.splash
 
 import android.os.Bundle
-
-import com.arellomobile.mvp.MvpAppCompatActivity
-import com.arellomobile.mvp.presenter.InjectPresenter
-import ru.mamykin.foboreader.presentation.splash.SplashPresenter
-
+import android.support.v7.app.AppCompatActivity
 import ru.mamykin.foboreader.ui.main.MainActivity
-import ru.mamykin.foboreader.presentation.splash.SplashView
 
 /**
  * Страница со сплэш-скрином
  */
-class SplashActivity : MvpAppCompatActivity(), SplashView {
-    @InjectPresenter
-    internal var presenter: SplashPresenter? = null
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // По-умолчанию View прикрепляется в onResume
-        mvpDelegate.onAttach()
-    }
-
-    override fun startApplication() {
         startActivity(MainActivity.getStartIntent(this))
         finish()
     }
