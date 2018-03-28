772cc0779709661ce595cab7b08c5248a2d3c7f2
= <=>
2018-03-29 00:48:29 +0500

    Refactor My Books screen - domain

--- app/src/main/java/ru/mamykin/foboreader/data/repository/BookRepository.kt
+++ app/src/main/java/ru/mamykin/foboreader/data/repository/BookRepository.kt
@@ -1,13 +1,32 @@
 package ru.mamykin.foboreader.data.repository
 
+import ru.mamykin.foboreader.data.database.BookDao
 import ru.mamykin.foboreader.data.database.BookDatabaseHelper
 import ru.mamykin.foboreader.data.model.FictionBook
+import rx.Completable
 import rx.Single
 import javax.inject.Inject
 
-class BookRepository @Inject constructor(private val bookDbHelper: BookDatabaseHelper) {
-
+class BookRepository @Inject constructor(
+        private val bookDbHelper: BookDatabaseHelper
+) {
     fun getBookInfo(bookId: Int): Single<FictionBook> {
         return Single.just(bookDbHelper.getBookDao()!!.getBook(bookId))
+    }
+
+    fun removeBook(bookId: Int): Completable {
+        return Completable.fromCallable {
+            val bookDao = bookDbHelper.getBookDao()!!
+            val book = bookDao.getBook(bookId)
+            return@fromCallable bookDao.delete(book)
+        }
+    }
+
+    fun getBooks(searchQuery: String, sortOrder: BookDao.SortOrder): Single<List<FictionBook>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun getBook(bookId: Int): Single<FictionBook> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
---
+++ app/src/main/java/ru/mamykin/foboreader/domain/mybooks/MyBooksInteractor.kt
@@ -0,0 +1,24 @@
+package ru.mamykin.foboreader.domain.mybooks
+
+import ru.mamykin.foboreader.data.database.BookDao
+import ru.mamykin.foboreader.data.model.FictionBook
+import ru.mamykin.foboreader.data.repository.BookRepository
+import rx.Completable
+import rx.Single
+import javax.inject.Inject
+
+class MyBooksInteractor @Inject constructor(
+        private val repository: BookRepository
+) {
+    fun removeBook(bookId: Int): Completable {
+        return repository.removeBook(bookId)
+    }
+
+    fun getBooks(searchQuery: String, sortOrder: BookDao.SortOrder): Single<List<FictionBook>> {
+        return repository.getBooks(searchQuery, sortOrder)
+    }
+
+    fun getBook(bookId: Int): Single<FictionBook> {
+        return repository.getBook(bookId)
+    }
+}
