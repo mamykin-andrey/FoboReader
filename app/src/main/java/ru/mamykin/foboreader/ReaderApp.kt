20f8e98d8d8b6379f4e0a508ee91e6eb0c1187d4
= <=>
2018-03-29 20:15:34 +0500

    Refactor App class

--- app/src/main/java/ru/mamykin/foboreader/ReaderApp.kt
+++ app/src/main/java/ru/mamykin/foboreader/ReaderApp.kt
@@ -1,57 +1,56 @@
 package ru.mamykin.foboreader
 
 import android.support.multidex.MultiDexApplication
-
-import ru.mamykin.foboreader.common.UiUtils
-import ru.mamykin.foboreader.di.AppComponent
-import ru.mamykin.foboreader.di.modules.AppModule
-import ru.mamykin.foboreader.di.DaggerAppComponent
-import ru.mamykin.foboreader.di.modules.PreferencesModule
-import ru.mamykin.foboreader.data.storage.PreferencesManager
-
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.core.CrashlyticsCore
-
+import io.fabric.sdk.android.Fabric
+import ru.mamykin.foboreader.common.UiUtils
+import ru.mamykin.foboreader.data.storage.PreferenceNames
+import ru.mamykin.foboreader.data.storage.PreferencesManager
+import ru.mamykin.foboreader.di.AppComponent
+import ru.mamykin.foboreader.di.modules.AppModule
+import ru.mamykin.foboreader.di.modules.PreferencesModule
 import javax.inject.Inject
 
-import io.fabric.sdk.android.Fabric
-import ru.mamykin.foboreader.data.storage.PreferenceNames
-
 class ReaderApp : MultiDexApplication(), PreferenceNames {
+
+    companion object {
+        lateinit var component: AppComponent
+            private set
+    }
+
     @Inject
-    lateinit var pm: PreferencesManager
+    lateinit var preferencesManager: PreferencesManager
 
     override fun onCreate() {
         super.onCreate()
-        setupAppComponent()
+        setupDagger()
         setupCrashlytics()
         setupTheme()
     }
 
     private fun setupCrashlytics() {
-        // Отключем Crashlytics для Debug-сборок
         val core = CrashlyticsCore.Builder()
-                .disabled(BuildConfig.DEBUG).build()
+                .disabled(BuildConfig.DEBUG)
+                .build()
+
         val crashlytics = Crashlytics.Builder()
                 .core(core)
                 .build()
+
         Fabric.with(this, crashlytics)
     }
 
     private fun setupTheme() {
-        UiUtils.nightMode = pm!!.getBoolean(PreferenceNames.Companion.NIGHT_THEME_PREF)
+        UiUtils.nightMode = preferencesManager.getBoolean(PreferenceNames.NIGHT_THEME_PREF)
     }
 
-    private fun setupAppComponent() {
+    private fun setupDagger() {
         component = DaggerAppComponent.builder()
                 .appModule(AppModule(this))
                 .preferencesModule(PreferencesModule())
                 .build()
-        component.inject(this)
-    }
 
-    companion object {
-        lateinit var component: AppComponent
-            private set
+        component.inject(this)
     }
 }
