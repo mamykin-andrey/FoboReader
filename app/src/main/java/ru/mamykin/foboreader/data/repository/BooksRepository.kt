7635fa97fa10a5ecf6a5e777e4f0b9d275dd7b2e
= <=>
2018-04-01 18:35:19 +0500

    Refactor BooksRepository

--- app/src/main/java/ru/mamykin/foboreader/data/repository/BooksRepository.kt
+++ app/src/main/java/ru/mamykin/foboreader/data/repository/BooksRepository.kt
@@ -1,22 +1,20 @@
 package ru.mamykin.foboreader.data.repository
 
 import ru.mamykin.foboreader.data.database.BookDao
-import ru.mamykin.foboreader.data.database.BookDatabaseHelper
 import ru.mamykin.foboreader.entity.FictionBook
 import rx.Completable
 import rx.Single
 import javax.inject.Inject
 
 class BooksRepository @Inject constructor(
-        private val bookDbHelper: BookDatabaseHelper
+        private val bookDao: BookDao
 ) {
     fun getBookInfo(bookId: Int): Single<FictionBook> {
-        return Single.just(bookDbHelper.getBookDao()!!.getBook(bookId))
+        return Single.just(bookDao.getBook(bookId))
     }
 
     fun removeBook(bookId: Int): Completable {
         return Completable.fromCallable {
-            val bookDao = bookDbHelper.getBookDao()!!
             val book = bookDao.getBook(bookId)
             return@fromCallable bookDao.delete(book)
         }
@@ -24,14 +22,12 @@ class BooksRepository @Inject constructor(
 
     fun getBooks(searchQuery: String, sortOrder: BookDao.SortOrder): Single<List<FictionBook>> {
         return Single.fromCallable {
-            val bookDao = bookDbHelper.getBookDao()!!
             return@fromCallable bookDao.getBooksList(searchQuery, sortOrder)
         }
     }
 
     fun getBook(bookId: Int): Single<FictionBook> {
         return Single.fromCallable {
-            val bookDao = bookDbHelper.getBookDao()!!
             val book = bookDao.getBook(bookId)
             book!!.lastOpen = System.currentTimeMillis()
             bookDao.update(book)
@@ -41,7 +37,6 @@ class BooksRepository @Inject constructor(
 
     fun getBook(bookPath: String): Single<FictionBook> {
         return Single.fromCallable {
-            val bookDao = bookDbHelper.getBookDao()!!
             val book = bookDao.getBook(bookPath)
             book.lastOpen = System.currentTimeMillis()
             bookDao.update(book)
