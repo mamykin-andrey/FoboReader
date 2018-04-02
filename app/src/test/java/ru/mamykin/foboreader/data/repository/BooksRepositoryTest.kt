bf873050e08258bedf8dbe98bc47d8de088952f8
= <=>
2018-04-02 10:09:56 +0500

    Add tests to BooksRepositoryTest

--- app/src/test/java/ru/mamykin/foboreader/data/repository/BooksRepositoryTest.kt
+++ app/src/test/java/ru/mamykin/foboreader/data/repository/BooksRepositoryTest.kt
@@ -27,11 +27,49 @@ class BooksRepositoryTest {
     }
 
     @Test
-    fun getBookInfo_returnsBookInfo_fromBookDao() {
+    fun getBookInfo_returnsBookInfoFromBookDao() {
         whenever(bookDao.getBook(bookId)).thenReturn(mockBook)
 
-        val testObserver = repository.getBookInfo(bookId).test()
+        val testSubscriber = repository.getBookInfo(bookId).test()
 
-        testObserver.assertCompleted().assertValue(mockBook)
+        testSubscriber.assertCompleted().assertValue(mockBook)
+    }
+
+    @Test
+    fun removeBook_removeBookFromBookDao() {
+        whenever(bookDao.getBook(bookId)).thenReturn(mockBook)
+
+        val testSubscriber = repository.removeBook(bookId).test()
+
+        testSubscriber.assertCompleted().assertNoErrors()
+    }
+
+    @Test
+    fun getBooks_returnsBooksFromBookDao() {
+        val mockBooks = listOf(mockBook, mockBook)
+        whenever(bookDao.getBooksList("", BookDao.SortOrder.BY_NAME)).thenReturn(mockBooks)
+
+        val testSubscriber = repository.getBooks("", BookDao.SortOrder.BY_NAME).test()
+
+        testSubscriber.assertCompleted().assertValue(mockBooks)
+    }
+
+    @Test
+    fun getBook_returnsBookFromBookDao_whenPassBookId() {
+        whenever(bookDao.getBook(bookId)).thenReturn(mockBook)
+
+        val testSubscriber = repository.getBook(bookId).test()
+
+        testSubscriber.assertCompleted().assertValue(mockBook)
+    }
+
+    @Test
+    fun getBook_returnsBookFromBookDao_whenPassBookPath() {
+        val bookPath = "/storage/1.fb"
+        whenever(bookDao.getBook(bookPath)).thenReturn(mockBook)
+
+        val testSubscriber = repository.getBook(bookPath).test()
+
+        testSubscriber.assertCompleted().assertValue(mockBook)
     }
 }
