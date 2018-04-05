97929c943da73d3f7861054b65d4a3c58089f44f
= <=>
2018-04-05 10:19:00 +0500

    Remove restoring saved state from MainActivity

--- app/src/main/java/ru/mamykin/foboreader/ui/main/MainActivity.kt
+++ app/src/main/java/ru/mamykin/foboreader/ui/main/MainActivity.kt
@@ -9,17 +9,36 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.mamykin.foboreader.R
+import ru.mamykin.foboreader.ui.booksstore.BooksStoreFragment
 import ru.mamykin.foboreader.ui.devicebooks.DeviceBooksFragment
 import ru.mamykin.foboreader.ui.dropbox.DropboxBooksFragment
 import ru.mamykin.foboreader.ui.global.BaseActivity
 import ru.mamykin.foboreader.ui.mybooks.MyBooksFragment
 import ru.mamykin.foboreader.ui.settings.SettingsActivity
-import ru.mamykin.foboreader.ui.booksstore.BooksStoreFragment
 
 /**
  * Основная страница, включает в себя страницу с книгами, файлами на устройстве, файлами Dropbox, магазином
  */
 class MainActivity : BaseActivity() {
+
+    companion object {
+
+        private const val MY_BOOKS_FRAGMENT_POS = 0
+        private const val DEVICE_BOOKS_FRAGMENT_POS = 1
+        private const val DROPBOX_BOOKS_FRAGMENT_POS = 2
+        private const val BOOKS_STORE_FRAGMENT_POS = 3
+
+        fun getHomeIntent(): Intent {
+            val homeIntent = Intent(Intent.ACTION_MAIN)
+            homeIntent.addCategory(Intent.CATEGORY_HOME)
+            homeIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            return homeIntent
+        }
+
+        fun getStartIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java)
+        }
+    }
 
     private lateinit var toggle: ActionBarDrawerToggle
 
@@ -29,7 +48,7 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
 
         initToolbar(getString(R.string.main_tab), true)
-        setupViewPager(viewpager, savedInstanceState)
+        setupViewPager(viewpager)
         tabLayout.setupWithViewPager(viewpager)
         setupDrawerLayout()
     }
@@ -55,24 +74,12 @@ class MainActivity : BaseActivity() {
         }
     }
 
-    private fun setupViewPager(viewPager: ViewPager, savedInstanceState: Bundle?) {
-        viewPager.offscreenPageLimit = TABS_COUNT
+    private fun setupViewPager(viewPager: ViewPager) {
         val adapter = MainViewPagerAdapter(supportFragmentManager)
-        if (savedInstanceState != null) {
-            adapter.addFragment(supportFragmentManager.getFragment(
-                    savedInstanceState, TAG_MY_BOOKS_FRAGMENT), getString(R.string.my_books))
-            adapter.addFragment(supportFragmentManager.getFragment(
-                    savedInstanceState, TAG_STORE_FRAGMENT), getString(R.string.store))
-            adapter.addFragment(supportFragmentManager.getFragment(
-                    savedInstanceState, TAG_DEVICE_BOOKS_FRAGMENT), getString(R.string.device))
-            adapter.addFragment(supportFragmentManager.getFragment(
-                    savedInstanceState, TAG_DROPBOX_BOOKS_FRAGMENT), getString(R.string.dropbox))
-        } else {
-            adapter.addFragment(MyBooksFragment.newInstance(), getString(R.string.my_books))
-            adapter.addFragment(DeviceBooksFragment.newInstance(), getString(R.string.device))
-            adapter.addFragment(DropboxBooksFragment.newInstance(), getString(R.string.dropbox))
-            adapter.addFragment(BooksStoreFragment.newInstance(), getString(R.string.store))
-        }
+        adapter.addFragment(MyBooksFragment.newInstance(), getString(R.string.my_books))
+        adapter.addFragment(DeviceBooksFragment.newInstance(), getString(R.string.device))
+        adapter.addFragment(DropboxBooksFragment.newInstance(), getString(R.string.dropbox))
+        adapter.addFragment(BooksStoreFragment.newInstance(), getString(R.string.store))
         viewPager.adapter = adapter
     }
 
@@ -83,6 +90,7 @@ class MainActivity : BaseActivity() {
             R.id.menu_my_books -> viewpager.currentItem = MY_BOOKS_FRAGMENT_POS
             R.id.menu_device -> viewpager.currentItem = DEVICE_BOOKS_FRAGMENT_POS
             R.id.menu_dropbox -> viewpager.currentItem = DROPBOX_BOOKS_FRAGMENT_POS
+            R.id.menu_books_store -> viewpager.currentItem = BOOKS_STORE_FRAGMENT_POS
             R.id.menu_settings -> startActivity(SettingsActivity.getStartIntent(this))
             R.id.menu_exit -> startActivity(MainActivity.getHomeIntent())
             else -> return super.onOptionsItemSelected(item)
@@ -96,28 +104,5 @@ class MainActivity : BaseActivity() {
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.app_name, R.string.app_name)
         toggle.isDrawerIndicatorEnabled = true
         drawerLayout.addDrawerListener(toggle)
-    }
-
-    companion object {
-
-        private const val TAG_MY_BOOKS_FRAGMENT = "tag_my_books_fragment"
-        private const val TAG_STORE_FRAGMENT = "tag_store_fragment"
-        private const val TAG_DEVICE_BOOKS_FRAGMENT = "tag_device_books_fragment"
-        private const val TAG_DROPBOX_BOOKS_FRAGMENT = "tag_dropbox_books_fragment"
-        private const val MY_BOOKS_FRAGMENT_POS = 0
-        private const val DEVICE_BOOKS_FRAGMENT_POS = 1
-        private const val DROPBOX_BOOKS_FRAGMENT_POS = 2
-        private const val TABS_COUNT = 4
-
-        fun getHomeIntent(): Intent {
-            val homeIntent = Intent(Intent.ACTION_MAIN)
-            homeIntent.addCategory(Intent.CATEGORY_HOME)
-            homeIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            return homeIntent
-        }
-
-        fun getStartIntent(context: Context): Intent {
-            return Intent(context, MainActivity::class.java)
-        }
     }
 }
