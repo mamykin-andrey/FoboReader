1d14bae4963f379d0ad4904fe60201ab7d981107
Mamykin Andrey <mamykin.av@bk.ru>
2020-10-01 20:44:41 +0500

    Fix Koin navigator binding

--- app/src/main/java/ru/mamykin/foboreader/app/ReaderApp.kt
+++ app/src/main/java/ru/mamykin/foboreader/app/ReaderApp.kt
@@ -4,16 +4,22 @@ import androidx.multidex.MultiDexApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import org.koin.dsl.binds
 import org.koin.dsl.module
 import ru.mamykin.foboreader.app.navigation.NavigatorImpl
 import ru.mamykin.foboreader.book_details.di.bookDetailsModule
+import ru.mamykin.foboreader.book_details.presentation.BookDetailsNavigator
 import ru.mamykin.foboreader.common_book_info.di.commonBookInfoModule
 import ru.mamykin.foboreader.core.di.coreModule
+import ru.mamykin.foboreader.core.platform.Navigator
 import ru.mamykin.foboreader.core.platform.NotificationUtils
 import ru.mamykin.foboreader.my_books.di.myBooksModule
+import ru.mamykin.foboreader.my_books.presentation.MyBooksNavigator
 import ru.mamykin.foboreader.read_book.di.readBookModule
 import ru.mamykin.foboreader.settings.di.settingsModule
 import ru.mamykin.foboreader.store.di.booksStoreModule
+import ru.mamykin.foboreader.store.presentation.BooksStoreNavigator
 
 @Suppress("unused")
 class ReaderApp : MultiDexApplication() {
@@ -26,11 +32,17 @@ class ReaderApp : MultiDexApplication() {
 
     private fun initDi() {
         startKoin {
-            androidLogger()
+            androidLogger(Level.DEBUG)
             androidContext(applicationContext)
-            modules(
-                listOf(
-                    module { single { NavigatorImpl() } },
+            modules(listOf(
+                    module {
+                        single { NavigatorImpl() }.binds(arrayOf(
+                                Navigator::class,
+                                BooksStoreNavigator::class,
+                                BookDetailsNavigator::class,
+                                MyBooksNavigator::class
+                        ))
+                    },
                     coreModule,
                     commonBookInfoModule,
                     myBooksModule,
@@ -38,8 +50,7 @@ class ReaderApp : MultiDexApplication() {
                     booksStoreModule,
                     settingsModule,
                     readBookModule
-                )
-            )
+            ))
         }
     }
 }
