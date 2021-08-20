0db618129218a2cbf58840936176a66f1605974f
mamykin-av <mamykin.av@bk.ru>
2021-08-20 10:28:03 +0300

    Update AGP

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 dependencies {
     // TODO: load from gradle.properties
     implementation("com.android.tools.build:gradle:4.1.0")
-    implementation("com.android.tools.build:gradle-api:4.1.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.10")
+    implementation("com.android.tools.build:gradle-api:4.1.3")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.21")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
 }
