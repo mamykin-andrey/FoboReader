30df10ca21f3c2d18205ac34e5227ffee2e64f6f
mamykin-av <mamykin.av@bk.ru>
2021-08-24 17:27:42 +0300

    Add build time analyzer plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ buildscript {
     repositories {
         jcenter()
         google()
+        maven(url = "https://plugins.gradle.org/m2/")
     }
     dependencies {
         val kotlinVersion: String by project
@@ -13,12 +14,32 @@ buildscript {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navigationVersion")
     }
 }
+
 allprojects {
     repositories {
         google()
         jcenter()
     }
 }
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+plugins {
+    id("net.rdrei.android.buildtimetracker") version "0.11.0"
+}
+
+buildtimetracker {
+    reporters {
+        register("summary") {
+            options["ordered"] = "true"
+            options["barstyle"] = "ascii"
+            options["shortenTaskNames"] = "false"
+        }
+        register("csv") {
+            options["output"] = "build/build_time.csv"
+            options["append"] = "true"
+        }
+    }
 }
