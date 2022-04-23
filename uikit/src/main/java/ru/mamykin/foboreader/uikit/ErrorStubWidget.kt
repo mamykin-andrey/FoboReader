71d4d3a9d2296e9462d0870196434d84495f01f3
mamykin-av <mamykin.av@bk.ru>
2022-04-23 11:43:04 +0300

    Refactor ErrorStubWidget

--- uikit/src/main/java/ru/mamykin/foboreader/uikit/ErrorStubWidget.kt
+++ uikit/src/main/java/ru/mamykin/foboreader/uikit/ErrorStubWidget.kt
@@ -2,53 +2,58 @@ package ru.mamykin.foboreader.uikit
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.Gravity
 import android.view.View
-import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.TextView
-import com.airbnb.lottie.LottieAnimationView
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import ru.mamykin.foboreader.core.extension.dpToPx
+import ru.mamykin.foboreader.core.extension.getLayoutInflater
 import ru.mamykin.foboreader.core.extension.safeThrow
+import ru.mamykin.foboreader.uikit.databinding.ViewErrorStubContentBinding
 
-// TODO: Decrease nesting of ViewGroups
 class ErrorStubWidget @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
-) : FrameLayout(context, attrs, defStyleAttr) {
+) : LinearLayout(context, attrs, defStyleAttr) {
 
     private var retryClickListener: (() -> Unit)? = null
-    private val tvErrorMessage: TextView
-    private val lavImage: LottieAnimationView
-    private val btnRetry: Button
+    private val binding = ViewErrorStubContentBinding.inflate(getLayoutInflater(), this)
+    private val contentPadding = context.dpToPx(12).toInt()
 
     init {
-        inflate(context, R.layout.view_error_stub_content, this)
-        tvErrorMessage = findViewById(R.id.tv_error_message)
-        lavImage = findViewById(R.id.lav_image)
-        btnRetry = findViewById(R.id.btn_retry)
-        initViews()
+        orientation = VERTICAL
+        setPadding(
+            contentPadding,
+            contentPadding,
+            contentPadding,
+            contentPadding
+        )
+        layoutParams = LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT,
+        )
+        gravity = Gravity.CENTER
+
+        binding.btnRetry.setOnClickListener {
+            retryClickListener?.invoke() ?: safeThrow(
+                IllegalStateException("Retry click listener not set!")
+            )
+        }
     }
 
     override fun setVisibility(visibility: Int) {
         super.setVisibility(visibility)
         if (visibility == View.VISIBLE) {
-            lavImage.playAnimation()
+            binding.lavImage.playAnimation()
         }
     }
 
     fun setMessage(message: String) {
-        tvErrorMessage.text = message
+        binding.tvErrorMessage.text = message
     }
 
     fun setRetryClickListener(retryClickListener: () -> Unit) {
         this.retryClickListener = retryClickListener
-    }
-
-    private fun initViews() {
-        btnRetry.setOnClickListener {
-            retryClickListener?.invoke() ?: safeThrow(
-                IllegalStateException("Retry click listener not set!")
-            )
-        }
     }
 }
